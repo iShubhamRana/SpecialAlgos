469124a4de249f6c983e28ad083b172bf5c35dc1
Shubham Rana <77272322+iShubhamRana@users.noreply.github.com>
2022-01-26 14:41:43 +0530

    Shortest job first scheduling algorithms

---
+++ SJF.cpp
@@ -0,0 +1,6 @@
+/*
+There are 2 shortest job first algorithms
+1. SJF1: https://www.geeksforgeeks.org/program-for-shortest-job-first-or-sjf-cpu-scheduling-set-1-non-preemptive/?ref=lbp
+2. SJF2: https://www.geeksforgeeks.org/program-for-shortest-job-first-sjf-scheduling-set-2-preemptive/?ref=lbp
+
+*/
