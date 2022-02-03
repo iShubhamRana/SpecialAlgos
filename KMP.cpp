c61da2cdd071656509bea3eac42ecd5a58042612
Shubham Rana <77272322+iShubhamRana@users.noreply.github.com>
2022-02-03 13:42:41 +0530

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -4,7 +4,7 @@ vector<int> prefix_function(string s) {
 	int n = s.length();
 	vector<int> pi(n, 0);
 	for (int i = 1; i < n; i++) {
-		int j = pi[i];
+		int j = pi[i-1];
 		while (j > 0 and s[j] != s[i])
 			j = p[j - 1];
 		if (s[i] == s[j])
