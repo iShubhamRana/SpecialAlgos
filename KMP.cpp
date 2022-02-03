8095b0321d59ba0f0b3dd97ad0f8b809b840406b
Shubham Rana <77272322+iShubhamRana@users.noreply.github.com>
2022-02-03 13:38:13 +0530

    KMP function

---
+++ KMP.cpp
@@ -0,0 +1,15 @@
+
+//function for prefix
+vector<int> prefix_function(string s) {
+	int n = s.length();
+	vector<int> pi(n, 0);
+	for (int i = 1; i < n; i++) {
+		int j = pi[i];
+		while (j > 0 and s[j] != s[i])
+			j = p[j - 1];
+		if (s[i] == s[j])
+			j++;
+		pi[i] = j;
+	}
+	return pi;
+}
